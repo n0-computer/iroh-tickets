1d2e6c5efbd95828f8d5ef506246bfb4b06ddb56
b5 <b5@users.noreply.github.com>
2025-10-25 13:10:47 +0000

    deploy: d6868d633679ae1440d684eb1ba27275bacdfea8

---
+++ pr/3/docs/trait.impl/core/fmt/trait.Debug.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_tickets",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"iroh_tickets/enum.ParseError.html\" title=\"enum iroh_tickets::ParseError\">ParseError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"iroh_tickets/endpoint/struct.EndpointTicket.html\" title=\"struct iroh_tickets::endpoint::EndpointTicket\">EndpointTicket</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[575]}
