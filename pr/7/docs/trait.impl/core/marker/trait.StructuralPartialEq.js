216d262e04ef57f35d5763a1c6d93f351d98dcd7
ramfox <ramfox@users.noreply.github.com>
2026-01-28 19:59:41 +0000

    deploy: 3880f4da368d25cfcc889b7f913ae65b087b00e3

--- pr/7/docs/trait.impl/core/marker/trait.StructuralPartialEq.js
+++ pr/7/docs/trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_tickets",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"iroh_tickets/endpoint/struct.EndpointTicket.html\" title=\"struct iroh_tickets::endpoint::EndpointTicket\">EndpointTicket</a>"]]]]);
+    const implementors = Object.fromEntries([["iroh_tickets",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"iroh_tickets/endpoint/struct.EndpointTicket.html\" title=\"struct iroh_tickets::endpoint::EndpointTicket\">EndpointTicket</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[363]}
+//{"start":59,"fragment_lengths":[365]}
