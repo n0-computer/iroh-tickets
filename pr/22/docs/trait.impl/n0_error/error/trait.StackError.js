c4a94621fbfe367bd8eb64daecf1e127694fdadf
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-05-07 15:25:56 +0000

    deploy: 7cf3482dc872a0d043fd05fcbebc211a20921485

---
+++ pr/22/docs/trait.impl/n0_error/error/trait.StackError.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["iroh_tickets",[["impl StackError for <a class=\"enum\" href=\"iroh_tickets/enum.ParseError.html\" title=\"enum iroh_tickets::ParseError\">ParseError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[160]}
